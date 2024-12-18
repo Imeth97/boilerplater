dd74cd5227e8b0eb4a69eb1c6b4aa803530e3a18
Imeth Don <ikah0003@student.monash.edu>
2024-12-18 21:32:21 +1100

    fix middleware issue

---
+++ src/app/api/auth/check-auth/route.ts
@@ -0,0 +1,7 @@
+import { auth } from "@/components/auth";
+import { NextResponse } from "next/server";
+
+export const GET = auth(function GET(req) {
+  if (req.auth) return NextResponse.json({ authenticated: !!req.auth });
+  return NextResponse.json({ authenticated: false }, { status: 401 });
+});
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,33 +1,25 @@
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
-import { auth } from "./components/auth";
+import { checkAuth } from "./components/auth/utils";
 
 // Routes that don't require authentication
 export const NO_AUTH_ROUTES = ["/"];
 
 // This function can be marked `async` if using `await` inside
 export async function middleware(req: NextRequest) {
-  try {
-    // Attempt to get the token from the request using NextAuth's getToken
-    const session = await auth();
-    const path = req.nextUrl.pathname;
-    // Check if the current path is in NO_AUTH_ROUTES
-    const isPublicPath = NO_AUTH_ROUTES.includes(path);
+  // Attempt to get the token from the request using NextAuth's getToken
+  const session = await checkAuth();
+  const path = req.nextUrl.pathname;
+  // Check if the current path is in NO_AUTH_ROUTES
+  const isPublicPath = NO_AUTH_ROUTES.includes(path);
 
-    if (!isPublicPath && !session) {
-      // User is not authenticated, redirect to the login page
-      return NextResponse.redirect(new URL("/", req.url));
-    }
-
-    // User is authenticated, proceed with the request
-    return NextResponse.next();
-  } catch (error) {
-    // Handle any unexpected errors
-    console.error("Error checking authentication:", error);
-
-    // Optionally, redirect to an error page or handle the error as needed
-    return NextResponse.redirect(new URL("/error", req.url));
+  if (!isPublicPath && !session) {
+    // User is not authenticated, redirect to the login page
+    return NextResponse.redirect(new URL("/", req.url));
   }
+
+  // User is authenticated, proceed with the request
+  return NextResponse.next();
 }
 
 // Configure which routes to run middleware on
