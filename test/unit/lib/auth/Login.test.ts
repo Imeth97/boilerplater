592601a60913eb791fedf7144d06f19617a61afb
Imeth Don <ikah0003@student.monash.edu>
2025-01-15 23:13:25 +1100

    tc fixes, updates

--- test/unit/lib/auth/Login.test.ts
+++ test/unit/lib/auth/Login.test.ts
@@ -1,6 +1,18 @@
+import db from "@/db/db";
 import { signIn } from "@/lib/auth";
 import Login from "@/lib/auth/Login";
 import { beforeEach, describe, expect, it, Mock, vi } from "vitest";
+
+vi.mock("@/db/db", () => ({
+  __esModule: true,
+  default: {
+    select: vi.fn(),
+  },
+}));
+
+vi.mock("drizzle-orm", () => ({
+  eq: vi.fn(),
+}));
 
 vi.mock("@/lib/auth", () => ({
   signIn: vi.fn(),
@@ -9,6 +21,20 @@ vi.mock("@/lib/auth", () => ({
 describe("Login function", () => {
   beforeEach(() => {
     vi.clearAllMocks();
+    (db.select as Mock).mockReturnValueOnce({
+      from: vi.fn().mockReturnValueOnce({
+        leftJoin: vi.fn().mockReturnValueOnce({
+          where: vi.fn().mockReturnValueOnce(
+            Promise.resolve([
+              {
+                provider: null,
+                password: null,
+              },
+            ])
+          ),
+        }),
+      }),
+    });
   });
 
   it("should return success: true for a valid login", async () => {
@@ -47,6 +73,63 @@ describe("Login function", () => {
       redirect: false,
       email: "",
       password: "",
+    });
+  });
+
+  it("if provider is not null and password is null, return success as false", async () => {
+    vi.clearAllMocks();
+    vi.resetAllMocks();
+    (db.select as Mock).mockImplementation(() => ({
+      from: vi.fn().mockReturnValue({
+        leftJoin: vi.fn().mockReturnValue({
+          where: vi.fn().mockReturnValue(
+            Promise.resolve([
+              {
+                provider: "github",
+                password: null,
+              },
+            ])
+          ),
+        }),
+      }),
+    }));
+
+    const response = await Login("test@test.com", "123456");
+
+    expect(response).toEqual({
+      success: false,
+      redirect: "/login?provider=github&email=test@test.com",
+    });
+    expect(signIn).not.toHaveBeenCalled();
+  });
+
+  it("if provider is not null and password is not null, attempt sign in with the provided credentials", async () => {
+    vi.clearAllMocks();
+    vi.resetAllMocks();
+    (db.select as Mock).mockImplementation(() => ({
+      from: vi.fn().mockReturnValue({
+        leftJoin: vi.fn().mockReturnValue({
+          where: vi.fn().mockReturnValue(
+            Promise.resolve([
+              {
+                provider: "github",
+                password: "blahblah",
+              },
+            ])
+          ),
+        }),
+      }),
+    }));
+
+    const response = await Login("test@test.com", "123456");
+
+    expect(response).toEqual({
+      success: true,
+    });
+    expect(signIn).toHaveBeenCalledWith("credentials", {
+      redirect: false,
+      email: "test@test.com",
+      password: "123456",
     });
   });
 });
